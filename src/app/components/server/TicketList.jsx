c9daf0c8aedc7054967cfe1407e61c318098d7e1
HADES248 <shivasingh24803@gmail.com>
2025-01-06 23:19:07 +0530

    fetch req error2

--- src/app/components/server/TicketList.jsx
+++ src/app/components/server/TicketList.jsx
@@ -9,6 +9,10 @@ async function getTickets() {
 
 
   const res = await fetch(`${baseUrl}/components/server/find`, {
+    method: "GET",
+    headers: {
+      'Content-Type': 'application/json'
+    },
     next: {
       revalidate: 0 // use 0 to opt out of using cache
     }
