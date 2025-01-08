65427d0acba9861609f4471796690907a506ba80
HADES248 <shivasingh24803@gmail.com>
2025-01-08 21:19:56 +0530

    fetch err 8

--- src/app/components/server/TicketList.jsx
+++ src/app/components/server/TicketList.jsx
@@ -1,27 +1,23 @@
-//import { connectToDatabase, ticketModel } from '../../../db/db'
 import Link from "next/link";
 // Important points:- 1. All of this code is server side component meaning this page is fully loaded before reaching the browser
 
-async function getTickets() {
-
+export async function getStaticProps() {
   try {
-    const res = await fetch("http://localhost:3000/components/server/find", {
-      next: {
-        revalidate: 0
-      }
-    });
+    const res = await fetch("http://localhost:3000/components/server/find");
 
     const { tickets } = await res.json();
-
-    return tickets;
+    return {
+      props: { tickets, }, revalidate: 0, // ISR
+    };
   } catch (error) {
     console.log(error);
+    return { props: { tickets: [], }, };
   }
 }
 
-export default async function TicketList() {
+export default async function TicketList({ tickets }) {
   // Creating an Instance of the function to get all the tickets.
-  const tickets = await getTickets();
+
   return (
     <>
       {tickets.length === 0 ? (
