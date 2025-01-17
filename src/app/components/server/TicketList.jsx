e37108e5428e811f8be775d1384c3cd02da9d704
HADES248 <shivasingh24803@gmail.com>
2025-01-17 18:45:05 +0530

    Using Cooking to render page dynamically

--- src/app/components/server/TicketList.jsx
+++ src/app/components/server/TicketList.jsx
@@ -1,20 +1,19 @@
+import { connectToDatabase, ticketModel } from "@/db/db";
+import { cookies } from "next/headers";
 import Link from "next/link";
 // Important points:- 1. All of this code is server side component meaning this page is fully loaded before reaching the browser
 
 async function getTickets() {
-  try {
-    const res = await fetch(`${ACCESS_URL}/components/server/find`, {
-      next: {
-        revalidate: 0
-      }
-    });
 
-    const { tickets } = await res.json();
+  cookies(); // This next js function is used to let next js know to render this page dynamically.
 
-    return tickets;
-  } catch (error) {
-    console.log(error);
-  }
+  //imitate delay
+  await new Promise(resolve => setTimeout(resolve, 1000));
+  await connectToDatabase();
+
+  const tickets = await ticketModel.find({});
+
+  return tickets;
 }
 
 export default async function TicketList() {
